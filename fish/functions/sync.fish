b2f7e87fb50a169684ec36c9ae5461ff69bd76a5
Mateus Etto <mateus.etto@proton.me>
2024-02-12 23:13:47 +0900

    Fish sync command use bold

--- fish/functions/sync.fish
+++ fish/functions/sync.fish
@@ -4,23 +4,25 @@ function sync --description 'Sync files with desktop'
         echo 'Pass the lftp arguments to connect to the desktop.' >&2
         return 1
     end
+    set bold (tput bold)
+    set reset (tput sgr0)
     set options --no-symlinks --ignore-time --delete --no-perms --exclude-glob='.*/' --verbose
     lftp -c "
         set cmd:fail-exit true;
         open $argv;
-        echo '(Phone > PC) Syncing backup';
+        echo $bold'(Phone > PC) Syncing backup'$reset;
         mirror $options --reverse $root/Backup/Phone/ Documents/Backup/Phone/;
-        echo '(PC > Phone) Syncing backup';
+        echo $bold'(PC > Phone) Syncing backup'$reset;
         mirror $options Documents/Backup/Computer/ $root/Backup/Computer/;
-        echo '(PC > Phone) Syncing music';
+        echo $bold'(PC > Phone) Syncing music'$reset;
         mirror $options Music/ $root/Music/;
-        echo '(PC > Phone) Syncing notes';
+        echo $bold'(PC > Phone) Syncing notes'$reset;
         mirror $options Documents/Notes/ $root/Notes/;
-        echo '(PC > Phone) Syncing pictures';
+        echo $bold'(PC > Phone) Syncing pictures'$reset;
         mirror $options Pictures/ $root/Pictures/;
-        echo '(PC > Phone) Syncing videos';
+        echo $bold'(PC > Phone) Syncing videos'$reset;
         mirror $options Videos/ $root/Videos/;
     "
-    echo 'Finish!'
+    echo $bold'Finish!'$reset
     return 0
 end

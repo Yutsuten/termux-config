688355b2c190863b1fddc33b809d118edb79e474
Mateus Etto <mateus.etto@proton.me>
2024-02-17 14:36:15 +0900

    Update fish config
    - Set CDPATH
    - Set fish color variables

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,13 @@ end
 
 if status is-interactive
     set -gx GPG_TTY (tty)
-    set -gx LESSOPEN $HOME'/.config/termux/less/lessopen.fish %s'
     set -gx LESSCLOSE $HOME'/.config/termux/less/lessclose.fish %s %s'
+    set -gx LESSOPEN $HOME'/.config/termux/less/lessopen.fish %s'
+
+    set -g CDPATH . $HOME $HOME/Projects
+    set -g fish_color_autosuggestion white
+    set -g fish_color_host brmagenta
+    set -g fish_color_host_remote yellow
+    set -g fish_color_user blue
     set -g fish_greeting
 end

5fcd9baf15636da2e0c14275a4b6b15cfc1626f1
Mateus Etto <mateus.etto@proton.me>
2024-03-31 17:50:10 +0900

    Fixes to termux/share.fish

--- termux/share.fish
+++ termux/share.fish
@@ -1,4 +1,4 @@
-#$PREFIX/usr/bin/env fish
+#!/usr/bin/env fish
 echo "How would you like to share '$argv[1]'?"
 echo '  1. Save to ~/downloads (default)'
 echo '  2. Save to ~/storage/internal/Download'
@@ -11,10 +11,13 @@ switch $answer
         return 0
     case 2
         mv $argv[1] ~/storage/internal/Download
+        return
     case 3
         mv $argv[1] ~/storage/sd-card/Download
+        return
     case 4
-        lftp $REMOTE/Downloads -c "put -E '$argv[1]'"
+        lftp -c "open $REMOTE/Downloads; put -E '$argv[1]'"
+        return
 end
 
 echo 'Defaulting to ~/downloads'

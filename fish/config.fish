913f1cfb5a205e55aaa1350e4b0d740e67467509
Mateus Etto <mateus.etto@proton.me>
2024-05-08 13:48:07 +0900

    Fish remove notes abbr, add cpwd abbr

--- fish/config.fish
+++ fish/config.fish
@@ -12,11 +12,11 @@ if status is-interactive
     set -g fish_color_user blue
     set -g fish_greeting
 
+    abbr --add cpwd     -- 'termux-clipboard-set (string replace --regex "^$HOME" \~ $PWD)'
     abbr --add identify -- 'identify -precision 3'
     abbr --add l1       -- 'ls -N1 --sort=v --group-directories-first'
     abbr --add ll       -- 'ls -Nlh --sort=v --group-directories-first'
     abbr --add lo       -- 'ls -Noh --sort=v --group-directories-first'
     abbr --add ls       -- 'ls -N --sort=v --group-directories-first'
     abbr --add n        -- 'nnn'
-    abbr --add notes    -- 'nvim -S ~/desktop/Notes.vim'
 end

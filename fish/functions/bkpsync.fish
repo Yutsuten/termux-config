18ada8c354a8a84c9a7152215487e5556b51bdce
Mateus Etto <mateus.etto@proton.me>
2024-07-13 15:50:01 +0900

    Update bkpsync option to upload media

--- fish/functions/bkpsync.fish
+++ fish/functions/bkpsync.fish
@@ -1,5 +1,5 @@
 function bkpsync --description 'Sync files with desktop'
-    argparse --ignore-unknown 'h/help' -- $argv
+    argparse --ignore-unknown 'h/help' 'u/upload' -- $argv
     set exitcode $status
 
     function help
@@ -10,6 +10,7 @@ function bkpsync --description 'Sync files with desktop'
         echo >&2
         echo '  Options:' >&2
         echo '    -h, --help      Show list of command-line options' >&2
+        echo '    -u, --upload    Upload media files to desktop' >&2
         echo >&2
         echo '  Positional arguments:' >&2
         echo "    LFTP_OPTIONS    Options to connect to the desktop's home directory" >&2
@@ -29,41 +30,55 @@ function bkpsync --description 'Sync files with desktop'
     set bold (tput bold)
     set reset (tput sgr0)
 
-    set rootdir ~/storage/sd-card/Sync
-    set options --dereference --ignore-time --delete --no-perms --verbose
+    set syncroot ~/storage/sd-card/Sync
+    set mediaroot ~/storage/internal
 
-    cp -a ~/.local/environment.fish $rootdir/Backup/Phone/environment.fish
-
-    function trim_old_backup
-        set keep_count $argv[1]
-        set cur 0
-        printf '%s\0' $argv[2..] | sort --zero-terminated --reverse | while read --null filename
-            set cur (math $cur + 1)
-            if test $cur -gt $keep_count
-                rm --force --verbose -- $filename
+    if set --query --local _flag_upload
+        lftp -c "
+            set cmd:fail-exit true;
+            open $argv;
+            set cmd:fail-exit false;
+            echo $bold'(Phone > PC) Uploading files'$reset;
+            mput -E -O Downloads/ $syncroot/Upload/*;
+            echo $bold'(Phone > PC) Uploading photos'$reset;
+            mput -E -O Downloads/ $mediaroot/DCIM/Camera/*;
+            echo $bold'(Phone > PC) Uploading screenshots'$reset;
+            mput -E -O Downloads/ $mediaroot/Pictures/Screenshots/*;
+        "
+    else  # Backup Sync
+        function trim_old_backup
+            set keep_count $argv[1]
+            set cur 0
+            printf '%s\0' $argv[2..] | sort --zero-terminated --reverse | while read --null filename
+                set cur (math $cur + 1)
+                if test $cur -gt $keep_count
+                    rm --force -- $filename
+                end
             end
         end
+
+        trim_old_backup 30 $syncroot/Backup/Phone/Contacts/*.vcf
+        trim_old_backup 30 $syncroot/Backup/Phone/Calendar/*.ics
+        cp -a ~/.local/environment.fish $syncroot/Backup/Phone/environment.fish
+
+        set options --dereference --ignore-time --delete --no-perms --verbose
+        lftp -c "
+            set cmd:fail-exit true;
+            open $argv;
+            echo $bold'(Phone > PC) Syncing backup'$reset;
+            mirror $options --reverse $syncroot/Backup/Phone/ Documents/Backup/Phone/;
+            echo $bold'(PC > Phone) Syncing documents'$reset;
+            mirror $options Documents/ $syncroot/Documents/;
+            echo $bold'(PC > Phone) Syncing music'$reset;
+            mirror $options Music/ $syncroot/Music/;
+            echo $bold'(PC > Phone) Syncing pictures'$reset;
+            mirror $options Pictures/ $syncroot/Pictures/;
+            echo $bold'(PC > Phone) Syncing videos'$reset;
+            mirror $options Videos/ $syncroot/Videos/;
+        "
+        echo $bold'Update media database'$reset
+        termux-media-scan -r $syncroot
+        echo $bold'Finish!'$reset
     end
-
-    trim_old_backup 30 $rootdir/Backup/Phone/Contacts/*.vcf
-    trim_old_backup 30 $rootdir/Backup/Phone/Calendar/*.ics
-
-    lftp -c "
-        set cmd:fail-exit true;
-        open $argv;
-        echo $bold'(Phone > PC) Syncing backup'$reset;
-        mirror $options --reverse $rootdir/Backup/Phone/ Documents/Backup/Phone/;
-        echo $bold'(PC > Phone) Syncing documents'$reset;
-        mirror $options Documents/ $rootdir/Documents/;
-        echo $bold'(PC > Phone) Syncing music'$reset;
-        mirror $options Music/ $rootdir/Music/;
-        echo $bold'(PC > Phone) Syncing pictures'$reset;
-        mirror $options Pictures/ $rootdir/Pictures/;
-        echo $bold'(PC > Phone) Syncing videos'$reset;
-        mirror $options Videos/ $rootdir/Videos/;
-    "
-    echo $bold'Update media database'$reset
-    termux-media-scan -r $rootdir
-    echo $bold'Finish!'$reset
     return 0
 end

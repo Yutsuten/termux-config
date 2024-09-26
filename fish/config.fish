6a254a8879a0567d2076382d261044bea98935a9
Mateus Etto <mateus.etto@proton.me>
2024-09-26 10:27:05 +0900

    Fish set environment variables on non-interactive run

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 test -f ~/.local/environment.fish && source ~/.local/environment.fish
 
+set -gx GPG_TTY (tty)
+set -gx LESSCLOSE $HOME'/.config/termux/less/lessclose.fish %s %s'
+set -gx LESSOPEN $HOME'/.config/termux/less/lessopen.fish %s'
+
 fish_add_path $HOME/.local/bin
 
 if status is-interactive
@@ -8,10 +12,6 @@ if status is-interactive
         exec ~/bin/todo
     end
 
-    set -gx GPG_TTY (tty)
-    set -gx LESSCLOSE $HOME'/.config/termux/less/lessclose.fish %s %s'
-    set -gx LESSOPEN $HOME'/.config/termux/less/lessopen.fish %s'
-
     set -g CDPATH . $HOME $HOME/projects
     set -g fish_color_autosuggestion white
     set -g fish_color_host brmagenta
@@ -19,13 +19,13 @@ if status is-interactive
     set -g fish_color_user blue
     set -g fish_greeting
 
-    abbr --add calc     -- 'bc -l'
-    abbr --add cpwd     -- 'termux-clipboard-set (string replace --regex "^$HOME" \~ $PWD)'
+    abbr --add calc -- 'bc -l'
+    abbr --add cpwd -- 'termux-clipboard-set (string replace --regex "^$HOME" \~ $PWD)'
     abbr --add identify -- 'identify -precision 3'
-    abbr --add l1       -- 'ls -N1 --sort=v --group-directories-first'
-    abbr --add ll       -- 'ls -Nlh --sort=v --group-directories-first'
-    abbr --add lo       -- 'ls -Noh --sort=v --group-directories-first'
-    abbr --add n        -- 'nnn'
+    abbr --add l1 -- 'ls -N1 --sort=v --group-directories-first'
+    abbr --add ll -- 'ls -Nlh --sort=v --group-directories-first'
+    abbr --add lo -- 'ls -Noh --sort=v --group-directories-first'
+    abbr --add n -- nnn
 
     if set --query NVIM
         abbr --add nvim -- 'nvim --server $NVIM --remote'
